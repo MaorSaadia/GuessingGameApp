172bf795fa00d6047509fa35f69c667dfb20e5cf
Maor <maorsa9@gmail.com>
2023-07-23 14:04:07 +0300

    working with the StartGameScreen Costumize it and added Buttom to enter number

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,11 +1,47 @@
-import { View, Text } from 'react-native';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
+
+function pressHandler() {}
 
 function PrimaryButton({ children }) {
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        // style={({ pressed }) =>
+        //   pressed
+        //     ? [styles.buttonInnerContainer, styles.pressed]
+        //     : styles.buttonInnerContainer
+        // }
+        style={styles.buttonInnerContainer}
+        onPress={pressHandler}
+        android_ripple={{ color: '#7a415d' }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
     </View>
   );
 }
 
 export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: 'hidden',
+  },
+
+  buttonInnerContainer: {
+    backgroundColor: '#72063c',
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  // For ios
+  //   pressed: {
+  //     opacity: 0.75,
+  //   },
+});
